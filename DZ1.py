1ee7a80c565043de84a057eb8f874f36246a5814
GolbanEF <golban.ef@yandex.ru>
2022-05-17 23:14:36 +0300

    Gotovo

---
+++ DZ1.py
@@ -0,0 +1,33 @@
+def hero_iq(heroes, link):
+    intelligence = 0
+    smart_hero = list()
+    for hero in heroes:
+        url = link + hero
+        req = requests.get(url)
+        hero_info = req.json()
+        if int(hero_info['results'][0]['powerstats']['intelligence']) > intelligence:
+            iq = int(hero_info['results'][0]['powerstats']['intelligence'])
+            smart_hero.clear()
+            smart_hero.append(hero_info['results'][0]['name'])
+        elif int(hero_info['results'][0]['powerstats']['intelligence']) == intelligence:
+            smart_hero.append(hero_info['results'][0]['name'])
+
+    return smart_hero, iq
+
+
+def main():
+    heroes = ['Hulk', 'Captain America', 'Thanos']
+    site = 'https://www.superheroapi.com/api/'
+    api = '2619421814940190'
+    link = site + api + '/search/'
+
+    smart_hero, iq = hero_iq(heroes, link)
+
+    print(f"Максимальный показатель интеллекта {iq}: ")
+    for name in smart_hero:
+        print(name)
+
+
+if __name__ == '__main__':
+    import requests
+    main()
